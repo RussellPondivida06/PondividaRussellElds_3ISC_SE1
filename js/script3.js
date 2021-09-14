f711777053d4ea04f6aa30dcc11bddfac2be5069
Russell Pondivida <russellpondivida@Russells-MacBook-Pro.local>
2021-09-14 09:32:41 +0800

    added script3.js

---
+++ js/script3.js
@@ -0,0 +1,11 @@
+const middle = [];
+    if (array.length >=1){
+        if (array.length %2 != 0){
+            return confirm [array.length/2];
+        }
+        else if(array.length %2 == 0){
+            return array[array.length/2];
+        }
+        return "";
+    }
+    else return "";  
